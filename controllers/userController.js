419ed0744bbba613079bf0c6809bdad6e8228b77
umairaddu <mohammedumair9718@gmail.com>
2024-07-20 15:22:26 +0530

    Your commit message

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import { response } from "express"
 import UserModel from "../models/userModel.js"
 
 
@@ -20,30 +21,86 @@ let usercreate = async (req, res) => {
 }
 
 
-const getUserbyId = async (req, res) => {
+// const getUserbyId = async (req, res) => {
 
+//     try {
+//         const id = req.params.id
+//         // console.log(id);
+//         const userData = await UserModel.findById({id})
+//         console.log(userData);
+//         if(!userData){
+//             throw new Error('Invalid credential')
+//         }
+//         const {Password,Number,...data} = userData._doc
+//         res.status(200).json({
+//             success: true,
+//             userData:data
+//         })
+//         console.log(userData);
+//     } catch (error) {
+//         res.status(404).json({
+//             success:false,
+//             message:error
+//         })
+//         console.log(`error in user get controller ${error}`);
+//     }
+// }
+const getUserbyId = async (req, res) => {
     try {
-        const id = req.params.id
+        const id = req.params.id;
         console.log(id);
-        const userData = await UserModel.findOne({name:name})
-        if(!userData){
-            throw new Error('Invalid credential')
+        const userData = await UserModel.findById(id);
+        if (!userData) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
         }
-        const {Password,Number,...data} = userData._doc
+
+        const { Password, Number, ...data } = userData._doc;
         res.status(200).json({
             success: true,
-            userData:data
-        })
+            userData: data
+        });
         console.log(userData);
-    } catch (error) {
-        res.status(404).json({
-            success:false,
-            message:error
-        })
-        console.log(`error in user get controller ${error}`);
+    }catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        });
+        console.log(`Error in getUserById controller: ${error}`);
     }
-}
+};
 
 
 
-export { usercreate, getUserbyId }
+const deleteuserbyid = async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(id);
+
+        const userdata = await UserModel.findByIdAndDelete(id);
+        if (!userdata) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+       return res.status(200).json({
+            success: true,
+            message: 'User deleted successfully'
+        });
+    } catch (error) {
+        console.log(`Error in deleteUserById controller: ${error}`);
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        });
+    }
+};
+
+
+
+
+export { usercreate, getUserbyId, deleteuserbyid }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getUserbyId, usercreate } from "../controllers/userController.js"
+import { deleteuserbyid, getUserbyId, usercreate } from "../controllers/userController.js"
  
 let userRouter=express.Router()
 
 
 userRouter.post('/usercreate',usercreate)
 userRouter.get('/getUserById/:id',getUserbyId)
+userRouter.delete('/deleteUserBy/:id',deleteuserbyid)
 
 export default userRouter
